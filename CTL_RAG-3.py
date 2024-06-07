c3639c40445a2efa54487f9196f76fa3b3775a66
Fengbujue862 <2893236615@qq.com>
2024-06-07 15:16:52 +0800

    gen CTL

--- CTL_RAG-3.py
+++ CTL_RAG-3.py
@@ -90,9 +90,9 @@ verif_prompt = get_prompt_from_file("verification_design.txt")
 patterns_file = get_prompt_from_file("patterns_p1_design.txt")
 
 ctl_str0 = "不要用中文，注意变量设计符合规范，请根据精化后需求进行分析后仅输出基于其用例的约束（注意将每个用例的事件流总结，包括基本流和备选流，并且结合用例场景和各种条件都整合成一个独立约束）对应的CTL/LTL（一个约束对应一个CTL/LTL,语法需要符合SMV模型检验），并且每条CTL/LTL之前标明对应的约束类型:"+'CTL设计原则(先判断需要用到哪些，再输出需要的约束类型):'+verif_prompt+"精化后的需求："+usecase_design
-ctl_str = "请根据原始的需求输出基于其本身的约束进行适当分析,原始需求文本中的每一个约束都要有一个CTL或LTL的形式（用验证smv模型的CTL/LTL语句来实现，不要用别的符号），不要中文回答:"+"原始需求："+raw_requirement+"CTL设计原则(先判断约束的类型，再给出对应的CTL或LTL范式)："+verif_prompt
+
 ctl_str1 = "不要用中文，且变量设计符合规范，一个事件流若包含多个事件则必须拆分，变量也必须尽量拆分，不做合并处理，请根据CTL/LTL知识库以及patterns文件中的内容设计的性质规约例如：{1},{2}等，注意不能有遗漏，每个变量名称单独定义的同时考虑互斥问题【如关灯，开灯即作为互斥变量】，一条patterns的对应一条CTL，注意最后结果仅含CTL范式以及对应patterns的类型（记得一定要去掉最前面的“EoC”或“PLC_END”,'PLC_START',等关键词；记得用尽量强的约束描述。如AG,AX，AF等，视情况决定，不要用GBK格式）：" + "patterns文件" + patterns_file + "格式文件："+ ctl_output
-user_input = input("请输入0或1来选择查询字符串（0为ctl_str0，1为ctl_str1）：")
+user_input = input("if use Refinement input 1 else input 0")
 if user_input == '0':
     query_str = ctl_str0
     file_name = 'ctl_design0.txt'
@@ -100,7 +100,7 @@ elif user_input == '1':
     query_str = ctl_str1
     file_name = 'ctl_design1.txt'
 else:
-    print("无效输入，请输入0或1。")
+    print("error input，please input 0 or 1")
     exit()
 
 # Use the query engine to execute the query
